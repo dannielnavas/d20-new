2b9d69f337b685da78693190173bf6935f733956
Danniel Navas <dannielnavas@gmail.com>
2026-05-12 12:58:13 -0500

    feat: actualizar URLs de API y socket en los entornos de producción y desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com',
-  socketUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com',
+  apiUrl: 'https://smiling-winny-dannieldev-596bcab2.koyeb.app',
+  socketUrl: 'https://smiling-winny-dannieldev-596bcab2.koyeb.app',
   discordClientId: '1503499096659591218',
   discordActivityEnabled: true,
 } as const;
