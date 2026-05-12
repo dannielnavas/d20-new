f4e5356c6c3c4524478f8fc3df25a2d760b165ca
Danniel Navas <dannielnavas@gmail.com>
2026-05-11 19:44:37 -0500

    feat: actualizar configuración de entorno para producción y desarrollo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com ',
-  socketUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com ',
-  discordClientId: '',
-  discordActivityEnabled: false,
+  apiUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com',
+  socketUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com',
+  discordClientId: '1503499096659591218',
+  discordActivityEnabled: true,
 } as const;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000",
-  socketUrl: "http://localhost:3000",
-  discordClientId: "1503499096659591218",
-  discordActivityEnabled: false,
+  apiUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com',
+  socketUrl: 'https://wiley-equal-bringing-sig.trycloudflare.com',
+  discordClientId: '1503499096659591218',
+  discordActivityEnabled: true,
 } as const;
