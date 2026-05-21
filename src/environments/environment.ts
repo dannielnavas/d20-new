66b9b67df5222927a085e130695825682abb8078
Danniel Navas <dannielnavas@gmail.com>
2026-05-21 10:20:39 -0500

    chore: update API URL to HTTPS and configure production environment file replacement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://smiling-winny-dannieldev-596bcab2.koyeb.app',
+  apiUrl: 'https://smiling-winny-dannieldev-596bcab2.koyeb.app',
   socketUrl: 'https://smiling-winny-dannieldev-596bcab2.koyeb.app',
   discordClientId: '1503499096659591218',
   discordActivityEnabled: true,
