47c6be09e199c887e5aa43ba4f2eed56e9dd09b9
Danniel Navas <dannielnavas@gmail.com>
2026-05-21 20:22:03 -0500

    feat: add new endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001',
-  socketUrl: 'http://localhost:3001',
+  apiUrl: 'https://smiling-winny-dannieldev-596bcab2.koyeb.app',
+  socketUrl: 'https://smiling-winny-dannieldev-596bcab2.koyeb.app',
   discordClientId: '1503499096659591218',
   discordActivityEnabled: true,
 } as const;
